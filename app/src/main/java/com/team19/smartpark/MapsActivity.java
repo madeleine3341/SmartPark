0580d843c7da1b6ca0168ba71098d28df711cf4c
Hao <tranhao3009@gmail.com>
2021-04-01 17:18:28 -0400

    Implement map filter and update map UI

--- app/src/main/java/com/team19/smartpark/MapsActivity.java
+++ app/src/main/java/com/team19/smartpark/MapsActivity.java
@@ -1,8 +1,10 @@
 package com.team19.smartpark;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
@@ -10,14 +12,20 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -31,20 +39,24 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.team19.smartpark.adapters.filterListAdapter;
 import com.team19.smartpark.models.Parking;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -52,12 +64,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int DEFAULT_ZOOM = 15;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private final Map<String, Marker> mMarkerMap = new HashMap<>();
+    private LinkedHashMap<String, Parking> parkingsList = new LinkedHashMap<String, Parking>();
     // A default location (Sydney, Australia) and default zoom to use when location permission is
     // not granted.
     private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
     private DatabaseReference mDatabase;
     private GoogleMap mMap;
     private SearchView searchView;
+    private ImageView closeButton;
+
+    private Context context;
+    private LinearLayout linearLayout;
+    private ListView filterListView;
+    private LinearLayoutManager llm;
+    private Button nearbyButton, clearButton;
+    private ToggleButton sDistanceButton, sASButton;
+    private TextView textView;
+    private LatLng cameraLatLng;
+    private BottomSheetBehavior mbottomSheetBehavior;
     // The entry point to the Fused Location Provider.
     private FusedLocationProviderClient fusedLocationProviderClient;
     private boolean locationPermissionGranted;
@@ -82,6 +106,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Construct a FusedLocationProviderClient.
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        filterUISetup();
         fab = findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,10 +121,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             TextView textView = searchView.findViewById(id);
             textView.setTextColor(Color.BLACK);
 
+
         } catch (NullPointerException ignore) {
 
         }
         searchView.onWindowFocusChanged(false);
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateFilterUI(false);
+                nearbyButton.setVisibility(View.GONE);
+            }
+        });
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                updateFilterUI(false);
+                return false;
+            }
+        });
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -120,7 +160,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                     }
                     if (address != null) {
+                        nearbyButton.setVisibility(View.VISIBLE);
                         LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                        cameraLatLng = latLng;
                         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
                     } else {
                         Toast.makeText(getApplicationContext(), "Adress not found", Toast.LENGTH_SHORT).show();
@@ -132,6 +174,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                updateFilterUI(false);
+                nearbyButton.setVisibility(View.GONE);
                 return false;
             }
         });
@@ -173,7 +217,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 Iterable<DataSnapshot> parkings = dataSnapshot.getChildren();
                 //array list to hold all parking lots
-                LinkedHashMap<String, Parking> parkingsList = new LinkedHashMap<String, Parking>();
                 for (DataSnapshot parking :
                         parkings) {
                     parking.getKey();
@@ -301,6 +344,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
                                         new LatLng(lastKnownLocation.getLatitude(),
                                                 lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                                cameraLatLng = new LatLng(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude());
                             }
                         } else {
                             Log.d(TAG, "Current location is null. Using defaults.");
@@ -315,6 +359,145 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } catch (SecurityException e) {
             Log.e("Exception: %s", e.getMessage(), e);
         }
+    }
+
+    private void filterUISetup(){
+        linearLayout = (LinearLayout) findViewById(R.id.bottomSheet);
+        filterListView = (ListView) findViewById(R.id.filterListView);
+        mbottomSheetBehavior = BottomSheetBehavior.from(linearLayout);
+        nearbyButton = findViewById(R.id.nearbyButton);
+        textView = findViewById(R.id.sortTextView);
+        sDistanceButton = findViewById(R.id.sortDistanceButton);
+        sASButton = findViewById(R.id.sortPriceButton);
+        clearButton = findViewById(R.id.clearButton);
+        updateFilterUI(false);
+
+        sDistanceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!sDistanceButton.isChecked() && !sASButton.isChecked()) {
+                    sDistanceButton.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+                    sASButton.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+                    mbottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+                else if(!sDistanceButton.isChecked()){
+                    sDistanceButton.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+                }
+                else{
+                    sDistanceButton.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(235,236,246)));
+                    sortAlgorithm();
+                    mbottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+            }
+        });
+        sASButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!sDistanceButton.isChecked() && !sASButton.isChecked()) {
+                    sDistanceButton.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+                    sASButton.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+                    mbottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+                else if(!sASButton.isChecked()){
+                    sASButton.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+                }
+                else{
+                    sASButton.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(235,236,246)));
+                    sortAlgorithm();
+                    mbottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+            }
+        });
+        nearbyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateFilterUI(true);
+            }
+        });
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateFilterUI(false);
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(16));
+            }
+        });
+    }
+    private void updateFilterUI(Boolean state){
+        if(state){
+            nearbyButton.setVisibility(View.GONE);
+            sDistanceButton.setVisibility(View.VISIBLE);
+            sASButton.setVisibility(View.VISIBLE);
+            textView.setVisibility(View.VISIBLE);
+            clearButton.setVisibility(View.VISIBLE);
+            linearLayout.setVisibility(View.VISIBLE);
+
+        }
+        else{
+            nearbyButton.setVisibility(View.VISIBLE);
+            sDistanceButton.setVisibility(View.GONE);
+            sASButton.setVisibility(View.GONE);
+            textView.setVisibility(View.GONE);
+            clearButton.setVisibility(View.GONE);
+            linearLayout.setVisibility(View.GONE);
+            sASButton.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+            sASButton.setChecked(false);
+            sDistanceButton.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+            sDistanceButton.setChecked(false);
+            mbottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        }
+    }
+    private void sortAlgorithm() {
+        float result1[] = new float[1];
+        float result2[] = new float[1];
+        ArrayList<Parking> filter = new ArrayList<Parking>();
+        ArrayList<String> distance = new ArrayList<String>();
+        TreeMap<String, Parking> fList = new TreeMap<>();
+        for (Map.Entry<String, Parking> parkingSet : parkingsList.entrySet()) {
+            Location.distanceBetween(cameraLatLng.latitude, cameraLatLng.longitude, parkingSet.getValue().lat, parkingSet.getValue().lng, result1);
+            if(result1[0] < 2000){
+            filter.add(parkingSet.getValue());
+            }
+        }
+        // Distance Sort
+            for (int i = 0; i < filter.size(); i++) {
+                for (int j = i + 1; j < filter.size(); j++) {
+                    if (sDistanceButton.isChecked() && !sASButton.isChecked()) {
+                        Location.distanceBetween(cameraLatLng.latitude, cameraLatLng.longitude, filter.get(i).lat, filter.get(i).lng, result1);
+                        Location.distanceBetween(cameraLatLng.latitude, cameraLatLng.longitude, filter.get(j).lat, filter.get(j).lng, result2);
+                        if (result1[0] > result2[0]) {
+                            Collections.swap(filter, j, i);
+                        }
+                    }
+                    else if (!sDistanceButton.isChecked() && sASButton.isChecked()) {
+                        if(Collections.frequency(filter.get(i).spots.values(), true) < Collections.frequency(filter.get(j).spots.values(), true)){
+                            Collections.swap(filter, i, j);
+                        }
+                    }
+                    else if(sDistanceButton.isChecked() && sASButton.isChecked()){
+                        Location.distanceBetween(cameraLatLng.latitude, cameraLatLng.longitude, filter.get(i).lat, filter.get(i).lng, result1);
+                        float spot1 = Collections.frequency(filter.get(i).spots.values(), true)/filter.get(i).spots.size();
+                        Location.distanceBetween(cameraLatLng.latitude, cameraLatLng.longitude, filter.get(j).lat, filter.get(j).lng, result2);
+                        float spot2 = Collections.frequency(filter.get(j).spots.values(), true)/filter.get(j).spots.size();
+                        float score1 = (float) (0.8*(1-result1[0]/2000) + 0.2*spot1);
+                        float score2 = (float) (0.8*(1-result2[0]/2000) + 0.2*spot2);
+                        if(score1>score2){
+                            Collections.swap(filter,j,i);
+                        }
+                    }
+
+                }
+            }
+
+        for (int i = 0; i < filter.size(); i++) {
+            Location.distanceBetween(cameraLatLng.latitude, cameraLatLng.longitude, filter.get(i).lat, filter.get(i).lng, result1);
+            distance.add(String.format("%.0f", result1[0]));
+        }
+        filterListAdapter adapter = new filterListAdapter(this, R.layout.adapter_bottom_sheet_list_view, filter, distance);
+        filterListView.setAdapter(adapter);
+    }
+
+    public void updateview(LatLng latLng){
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
     }
 }
 
---
+++ app/src/main/java/com/team19/smartpark/adapters/filterListAdapter.java
@@ -0,0 +1,80 @@
+package com.team19.smartpark.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.team19.smartpark.MapsActivity;
+import com.team19.smartpark.R;
+import com.team19.smartpark.models.Parking;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+
+public class filterListAdapter extends ArrayAdapter<String> {
+    private int mylayout;
+    private Context fcontext;
+    private ArrayList<Parking> parkingInfo = new ArrayList<Parking>();
+    private ArrayList<String> fdistance = new ArrayList<String>();
+    private ArrayList<String> avSpots = new ArrayList<String>();
+
+    public filterListAdapter(Context context, int resource, ArrayList<Parking> parkingInfo, ArrayList<String> distance) {
+        super(context, resource);
+        mylayout = resource;
+        fcontext = context;
+        this.parkingInfo = parkingInfo;
+        fdistance = distance;
+        for(int i =0;i<parkingInfo.size();i++) {
+            String avs = "";
+            for(Map.Entry<String,Boolean> key: parkingInfo.get(i).spots.entrySet()){
+                if(key.getValue()){
+                    avs = avs + key.getKey() + " ";
+                }
+            }
+            avSpots.add(avs);
+        }
+    }
+    public int getCount() {
+        return parkingInfo.size();
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater = LayoutInflater.from(fcontext);
+        convertView = inflater.inflate(mylayout,parent,false);
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(fcontext, parkingInfo.get(position).name, Toast.LENGTH_SHORT).show();
+                LatLng latLng = new LatLng(parkingInfo.get(position).lat,parkingInfo.get(position).lng);
+                ((MapsActivity)fcontext).updateview(latLng);
+            }
+        });
+        TextView name = (TextView) convertView.findViewById(R.id.nameFilterTextView);
+        TextView address = (TextView) convertView.findViewById(R.id.addressFilterTextView);
+        TextView status = (TextView) convertView.findViewById(R.id.statusFilterTextView);
+        TextView distance = (TextView) convertView.findViewById(R.id.distanceTextView2);
+        TextView availablespots = (TextView) convertView.findViewById(R.id.availableSpotTextView2);
+
+        name.setText(parkingInfo.get(position).name);
+        address.setText(parkingInfo.get(position).address);
+        String stat = null;
+        if(Collections.frequency(parkingInfo.get(position).spots.values(), true) > 0){
+            stat = "Available";
+        }
+        else{
+            stat = "Not Available";
+        }
+        status.setText("Status: " + stat);
+        distance.setText(fdistance.get(position) + " m");
+        availablespots.setText(avSpots.get(position));
+        return convertView;
+    }
+}
